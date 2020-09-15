96576de5d56cac85b3925d5ffc3834944121ae19
Heather Peterson <hpeterson462@gmail.com>
2020-09-15 16:09:50 -0700

    returning books from scarping

--- _tests_/parser.test.js
+++ _tests_/parser.test.js
@@ -1,12 +1,13 @@
-const request = require('../lib/request');
-const parse = require('../lib/parser');
+const request = require('../lib/scraper/request');
+const parse = require('../lib/scarper/parser');
 
 describe('parse function', () => {
   it('returns an array of all book names, ratings, price, and stock', async () => {
-    const document = await request();
+    const document = await request(1);
 
     const bookNames = parse(document);
 
+    expect(bookNames).toHaveLength(10);
     expect(bookNames).toEqual(expect.arrayContaining([
       {
         title: 'A Light in the Attic', image: 'catalogue/a-light-in-the-attic_1000/index.html', rating: 'star-rating Three', price: '£51.77', stock: true
